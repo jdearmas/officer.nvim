f6d8e925ab62adf16f4f41e43874b6f5b855a5c3
John De Armas <var.mail.john@gmail.com>
2024-12-15 13:48:25 -0800

    Update officer.lua
    lol

--- lua/officer.lua
+++ lua/officer.lua
@@ -7,6 +7,15 @@ local M = {}
 function M.spawn_cmd(cmd, params)
   local overseer = require("overseer")
 
+  overseer.setup({
+  -- Configure the task list
+    task_list = {
+      -- Default direction. Can be "left", "right", or "bottom"
+      direction = "bottom",
+    }
+  })
+
+
   local components = config.get_components(params)
 
   local task = overseer.new_task({
